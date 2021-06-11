fb556876027a38e9e52b2cfe834c3d90e92d509c
Viktorlazi <viktorlazi010@gmail.com>
2021-06-12 00:03:25 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {observer} from "mobx-react";
 
 if(!window.web3){
   alert("This app requires blockchain browser to run. Consider using metamask :)")
+  alert("Redirecting to metamask.io");
   window.location.href = "https://metamask.io/download";
 }
 

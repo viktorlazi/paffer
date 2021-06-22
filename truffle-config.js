72d9073b5bf89b89ad81499d76d1004bd7fc57ab
Viktorlazi <viktorlazi010@gmail.com>
2021-06-22 12:11:55 +0200

    contract on ropsten testnet

---
+++ truffle-config.js
@@ -0,0 +1,29 @@
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "fd56272923d04c0d90ad79c17f048fa9";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    }
+  },
+  contracts_directory: './Contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
